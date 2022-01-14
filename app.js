7d9610f4611b7e9141e65cc82a55a9b830d74c7c
Jimbok21 <jimmykells@sky.com>
2022-01-14 13:48:57 +0000

    sets status saying .help for help

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const Discord = require('discord.js');
 const mongoose = require('./database/mongoose');
 const { Client, Intents } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_TYPING, Intents.FLAGS.GUILD_MEMBERS] });
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+        Intents.FLAGS.GUILD_MEMBERS] });
 
     
 const fs = require('fs');
--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,6 @@ module.exports = {
     once: true,
     execute(client) {
         console.log(`${client.user.tag} has logged into Discord`);
+        client.user.setActivity(".help for help", {type: "PLAYING"});
     },
 };
