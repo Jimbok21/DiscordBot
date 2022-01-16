a06f644045c441bfcaf82124a23fe84e2767f92b
Jimbok21 <jimmykells@sky.com>
2022-01-16 15:42:14 +0000

    hardcoded get_question

--- commands/get_question.js
+++ commands/get_question.js
@@ -5,32 +5,30 @@ module.exports = {
 	description: 'gets the specific question and answer from the database',
 	async execute(message, args, client) {
 
+        message.channel.send("Please type the English of the question you want to find")
 
-        const filter = (m) => {
-            return m.author.id === message.author.id
-        };
-
-        message.channel.send("Please type the English of the question you want to find");
-        const collector = message.channel.createMessageCollector({
-            filter,
-            max: 1,
-            time: 5000,
-        })
-        let msg
-        
-        collector.on('collect', (message) => {
-            let profileData
-            msg = message.content
-            console.log("lemon")
-            console.log(msg)
-            //try {
+        // const filter = (m) => {
+        //     return m.author.id === message.author.id
+            
+        // };
+        // message.channel
+        //     .awaitMessages({filter, max: 5, time: 5000 })
+        //     .then((collected) => {
+        //         const msg = collected.first()
+        //         console.log("lemon")
+        //         console.log(msg)
+                
+        //     })
+        //     .catch((err) => console.log("ERROR " + err + msg))
+        let msg = "hi"
+            try {
                 profileData = await questionsModel.findOne({questionAnswer: 'hi'})
                 console.log(profileData.questionsTxt)
-            // } catch(err) {
-            //     console.log(err)
-            // }
-            // message.channel.send(profileData.questionTxt)
-            // message.channel.send(profileData.questionAnswer)
-        })
+             } catch(err) {
+                 console.log(err)
+             }
+             message.channel.send(profileData.questionTxt)
+             message.channel.send(profileData.questionAnswer)
+            
 	},
 };
