9c6817d837cad24c8d78eeadf822335328697ee8
Jimbok21 <jimmykells@sky.com>
2022-01-17 19:09:45 +0000

    the .start command now lets you do as many questions as you can in 30 seconds

--- commands/start.js
+++ commands/start.js
@@ -5,23 +5,21 @@ module.exports = {
     description: 'will ask the user a set of 5 random questions based on the difficulty they input',
     async execute(message, args, client) {
 
+        //initialize variables and send intro messages
         let profileData
         let selected = false
         let counter = 0
         let score = 0
-        message.channel.send("you have 30 seconds to answer 5 questions")
+        message.channel.send("you have 30 seconds to answer as many questions as you can")
         message.channel.send("What difficulty would you like? \nplease type: easy, medium, hard or random")
 
-        const filter = (m) => {
-            console.log(m.author.id)
-            return m.author.id === message.author.id
-        };
-
+        //sets the message collector to 30 seconds
         const collector = new DiscordJS.MessageCollector(message.channel, {
             time: 30000,
         })
 
         collector.on("collect", async (messg) => {
+            //checks to make sure the ID is from the person playing the game
             if (messg.author.id != message.author.id) return;
             if (selected === false) {
                 if (messg.content === "random") {
@@ -55,15 +53,15 @@ module.exports = {
                 //this only happens if the database does not have at least 5 questions for the chosen difficulty
                 //in practice this should never be needed because the database will have enough default questions
                 if(profileData[counter] == null) {
-                    message.channel.send(`All out of questions`)
+                    message.channel.send(`All out of questions, please wait for the timer to elapse`)
                     return
                 }
                 //checks if the answer is correct and adds it to the score
                 if(profileData[counter].questionTxt === answers[counter]) {
                     score++
-                    message.channel.send(`Correct, currently on ${score}/5`)
+                    message.channel.send(`Correct, currently on ${score}/${profileData.length}`)
                 } else {
-                    message.channel.send(`Incorrect, the answer was ${profileData[counter].questionTxt} \ncurrently on ${score}/5`)
+                    message.channel.send(`Incorrect, the answer was ${profileData[counter].questionTxt} \ncurrently on ${score}/${profileData.length}`)
                 }
 
                 //sends the next question
@@ -79,7 +77,7 @@ module.exports = {
 
         //sends well done when the game ends
         collector.on("end", (msg) => {
-            message.channel.send(`Well done! Your final score was ${score}/5`)
+            message.channel.send(`Well done! Your final score was ${score}/${profileData.length}`)
         })
 
         //will put the questions in the array into a random order
