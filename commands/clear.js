8708c0fa8c46820ebaaec5064fae5ae9fb4e1a7a
Jimbok21 <jimmykells@sky.com>
2022-03-25 18:34:43 +0000

    updated clear to remove 2-100 messages and changed some text

--- commands/clear.js
+++ commands/clear.js
@@ -1,14 +1,14 @@
 
 module.exports = {
 	name: 'clear',
-	description: 'Clears the number of messages that you input',
+	description: 'Clears the number of messages that you input, You can only delete messages that are under 14 days old and NOT in DMs',
     inputs: '<1-100>',
 	async execute(message, args, client) {
         if(!args[0]) return message.reply("please enter the number of messages you want deleted");
-        if(isNaN(args[0])) return message.reply("please enter a valid number below 100");
+        if(isNaN(args[0])) return message.reply("please enter a valid number between 2-100");
 
         if(args[0] > 100) return message.reply("you cannot delete more than 100 messages");
-        if(args[0] < 1) return message.reply("you must choose a number above 1");
+        if(args[0] <= 1) return message.reply("you must choose a number above 1");
         
         //waits to fetch all of the messages that the arguement specifies
         //when they've all been fetched, they're deleted.
@@ -17,7 +17,7 @@ module.exports = {
             message.channel.bulkDelete(messages, true);
         })
         } catch (error) {
-            message.channel.send("You can only delete messages that are under 14 days old or NOT in DMs");
+            message.channel.send("You can only delete messages that are under 14 days old and NOT in DMs");
             console.log(error);
         };
 	},
