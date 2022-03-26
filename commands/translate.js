f0d406badbea8bae6197ca8f69c69d788130d569
Jimbok21 <jimmykells@sky.com>
2022-03-26 14:38:37 +0000

    translate working with google link

--- commands/translate.js
+++ commands/translate.js
@@ -12,34 +12,33 @@ module.exports = {
         //creates a message collector
         const collector = new DiscordJS.MessageCollector(message.channel, {
             filter,
+            max: 1,
         })
-        let english = ""
+        
         if (!args[0]) {
+            //only sends if the user has not set an argument
             message.channel.send('please type the english of the word you want to translate')
         }
 
         if (args[0]) {
-            english = args[0]
-            translate(english, { from: 'en', to: 'zh-cn' }).then(res => {
-                message.channel.send(`**${english}** = **${res}**`)
+            //translates if the command contains the argument
+            translate(args[0], { from: 'en', to: 'zh-cn' }).then(res => {
+                message.channel.send(`\nto listen or see more detail, visit: https://translate.google.com/?hl=en&sl=en&tl=zh-CN&text=${args[0]}%0A&op=translate`)
+                message.channel.send(`**${args[0]}** = **${res}**\n`)
             }).catch(err => {
                 console.error(err)
             })
             collector.stop('end')
         } else {
+            //collects the message and translates it
             collector.on("collect", async (messg) => {
-                english = messg.content
-                collector.stop('end')
+                translate(messg.content, { from: 'en', to: 'zh-cn' }).then(res => {
+                    message.channel.send(`\nto listen or see more detail, visit: https://translate.google.com/?hl=en&sl=en&tl=zh-CN&text=${messg.content}%0A&op=translate`)
+                    message.channel.send(`**${messg.content}** = **${res}**\n`)
+                }).catch(err => {
+                    console.error(err)
+                })
             })
         }
-
-        collector.on("end", (msg) => {
-            translate(english, { from: 'en', to: 'zh-cn' }).then(res => {
-                message.channel.send(`**${english}** = **${res}**`)
-            }).catch(err => {
-                console.error(err)
-            })
-        })
-
     },
 }
